667511b292156c629d886061f3ed1f2d24dece65
Bryan Tan <techniux@gmail.com>
2017-11-10 20:26:24 -0800

    Refactor chat.js; hide chat log when not logged in

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -9,34 +9,43 @@ var sendMessage = function(type, data) {
     ws.send(JSON.stringify(payload));
 };
 
+var handleChatEvent = function(event) {
+    var atBottom = chatlog.scrollTop == chatlog.scrollTopMax;
+
+    var msg = event.message;
+    console.log("Received message from server:", event);
+    var child = document.createElement("li");
+    child.textContent = msg;
+    chatlog.appendChild(child);
+
+    // auto scroll if at bottom of chat log
+    if (atBottom) {
+        chatlog.scrollTop = chatlog.scrollHeight;
+    }
+};
+
+var connect = function(name) {
+    ws = new WebSocket("ws://" + location.host + "/chat");
+    ws.onmessage = function(e) {
+        handleChatEvent(JSON.parse(e.data));
+    };
+    ws.onclose = function() {
+        ws = null;
+    };
+    ws.onerror = function(e) {
+        console.log("Error:", e);
+    };
+    ws.onopen = function() {
+        sendMessage("Connect", { name: name });
+    };
+};
+
 document.getElementById("chat-submit").addEventListener("submit", function(e) {
     e.preventDefault();
     var msg = chatbox.value.trim();
     if (msg != "") {
         if (ws == null) {
-            ws = new WebSocket("ws://" + location.host + "/chat");
-            ws.onmessage = function(e) {
-                var data = JSON.parse(e.data);
-                var msg = data.message;
-                console.log("Received message from server:", data);
-                var atBottom = chatlog.scrollTop == chatlog.scrollTopMax;
-                var child = document.createElement("li");
-                child.textContent = msg;
-                chatlog.appendChild(child);
-
-                if (atBottom) {
-                    chatlog.scrollTop = chatlog.scrollHeight;
-                }
-            };
-            ws.onclose = function() {
-                ws = null;
-            };
-            ws.onerror = function(e) {
-                console.log("Error:", e);
-            };
-            ws.onopen = function() {
-                sendMessage("Connect", { name: msg });
-            };
+            connect(msg);
         } else {
             sendMessage("SendMessage", { message: msg });
         }
